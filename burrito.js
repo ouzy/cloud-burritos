8ea469e958332ac8c3d7f0c71c1f8386504bd980
ouzy <ousmane.mariko89@gmail.com>
2014-09-23 22:48:37 -0400

    initial commit

---
+++ burrito.js
@@ -0,0 +1,5 @@
+
+javascript:(function(){
+	document.body.innerHTML = document.body.innerHTML.replace(/cloud/g, "burrito");
+	document.body.innerHTML = document.body.innerHTML.replace(/Cloud/g, "Burrito");
+})();
